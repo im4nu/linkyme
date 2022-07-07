16a3cbe48343ffabf6d7aa18625816f91a006157
Emmanuel Victor Rodrigues de Lima <102077709+im4nu@users.noreply.github.com>
2022-07-07 12:04:11 -0300

    Create console.js

---
+++ console.js
@@ -0,0 +1,24 @@
+const btnEl = document.querySelector('.btn')
+
+const toggleOptions = () => {
+  const wrapperEl = document.querySelector('.wrapper')
+  const iconEl = btnEl.querySelector('i')
+
+  wrapperEl.classList.toggle('active')
+
+  if (iconEl.classList.contains('ri-share-line')) {
+    iconEl.classList.replace('ri-share-line', 'ri-close-line')
+  } else {
+    iconEl.classList.replace('ri-close-line', 'ri-share-line')
+  }
+}
+
+btnEl.addEventListener('click', toggleOptions)
+
+function darkMode() {
+  document.body.classList.add('dark')
+}
+
+function lightMode() {
+  document.body.classList.remove('dark')
+}
